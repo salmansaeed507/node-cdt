147c68f4606d59a104cda0f95d2a0eed8a0fb1e6
Salman Saeed <salmansaeed507@gmail.com>
2020-07-24 23:51:56 +0500

    reset index2.js

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-var http = require('http');
-const config = require('config');
+var express = require('express');
+var app = express();
 
-http.createServer(function(req,res){
+app.set('view engine','pug');
+app.set('views','./views');
+app.use(express.static('public'));
 
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write('Hello World');
-    res.end();
-
-}).listen(config.get('app.port'), config.get('app.host'),function(){
-    console.log("server running...");
+app.get('/', function(req, res){
+   res.render("index");
 });
+
+var server = app.listen(3000);
+
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+
+    socket.broadcast.emit('needcanvas');
+
+    var canvas = null;
+    socket.on('clientcanvas', function(data){
+        if (!canvas) {
+            canvas = data;
+            socket.broadcast.emit('updatecanvas',canvas);
+        }
+    });
+
+    socket.on('draw',function(data){
+        socket.broadcast.emit('draw',data);
+    });
+
+    socket.on('drawShape', function(data){
+        socket.broadcast.emit('drawShape',data);
+    });
+
+    socket.on('clear', function(){
+        socket.broadcast.emit('clear');
+    });
+    
+    socket.on('disconnect', function () {
+       console.log('A user disconnected');
+    });
+});
+
+ 
